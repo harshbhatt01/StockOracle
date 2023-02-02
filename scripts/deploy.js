cd77e92c33b2fefd690f3688a9c1fdf1cda93a34
Harsh Bhatt <harshbhatt0151@gmail.com>
2023-02-02 17:30:53 +0530

    ft.02

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,47 @@
-
 const hre = require("hardhat");
+const { Contract, ethers } = require('ethers');
+const axios = require('axios');
 
 async function main() {
 
-  const StockDataContract = await hre.ethers.getContractFactory("StockDataContract");
-  const _StockDataContract = await StockDataContract.deploy();
 
-  await _StockDataContract.deployed();
-  console.log(_StockDataContract.address)
+  const api = await hre.ethers.getContractFactory("StockDataContract");
+  const _api = await api.deploy();
+
+  await _api.deployed();
+
+  console.log(
+    "Address :", _api.address
+  );
+
+  const transactionResponse = await _api.requestStock("AAPL")
+  const transactionReceipt = await transactionResponse.wait()
+  console.log(transactionReceipt.events[0].args.name_stock)
+  // Name = transactionReceipt.events[0].args.name_stock
+
+
+  try {
+        Name = transactionReceipt.events[0].args.name_stock
+        const apiKey = 'uVkr9z1EwjmDoCJyI1XjeKFBo8KF5B1m';
+        const StockAPI = `https://api.polygon.io/v1/open-close/${Name}/2023-01-09?adjusted=true&apiKey=${apiKey}`
+        const response = await axios.get(StockAPI);
+        const StockData = response.data;
+        //console.log(PlayerData);
+        open_ = StockData.open.toString();
+        high_ = StockData.high.toString();
+        low_ = StockData.low.toString();
+        console.log(open_, high_, low_); 
+        
+        const updateData = await _api.updateWeather(Name,open_,high_,low_)
+        //console.log(updateData);
+        const finalData = await _api.getStock(Name)
+        console.log(finalData)
+
+  } catch (e) {
+    console.error(e);
+}
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
